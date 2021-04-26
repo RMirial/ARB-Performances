ecac6b07a7b0e66e56e3f797bdb2251b5a9112e5
RMirial <73707067+RMirial@users.noreply.github.com>
2021-04-26 19:08:47 +0200

    Add files via upload

---
+++ Formulaire.php
@@ -0,0 +1,75 @@
+
+<?php
+
+$name = $lastname = $email = $phone = $datedeb = $datefin = $message = "";
+
+if(isset($_POST['mailform'])) {
+   $name = verificationInput($_POST["name"]);
+   $lastname = verificationInput($_POST["lastname"]);
+   $email = verificationEmail($_POST["email"]);
+   $phone = verificationInput($_POST["phone"]);
+   $datedeb = verificationInput($_POST["deb-even"]);
+   $datefin = verificationInput($_POST["fin-even"]);
+   $message = verificationInput($_POST["message"]);
+
+   if(!empty($name) AND !empty($lastname) AND !empty($email) AND !empty($phone) AND !empty($datedeb) AND !empty($datefin) AND !empty($message)) {
+      
+      $header="MIME-Version: 1.0\r\n";
+      $header.='From:"arb-performances.fr"<contact@arb-performances.fr>'."\n";
+      $header.='Content-Type:text/html; charset="utf-8"'."\n";
+      $header.='Content-Transfer-Encoding: 8bit';
+      // Format dont le message va être reçu  
+      $message='
+      <html>
+         <body>
+            <div align="center">
+               <br />
+               <h1>
+               <u>Nom de l\'expéditeur :</u>'.$name.'</h1><br />
+               <h1>
+               <u>prénom de l\'expéditeur :</u>'.$lastname.'</h1><br />
+               <p><u>Mail de l\'expéditeur :</u>'.$email.'</p><br />
+               <p><u>Tel de l\'expéditeur :</u>'.$phone.'</p><br />
+               <p><u>Date de début de l\'évènement souhaité :</u>'.$datedeb.'</p><br />
+               <p><u>Date de fin de l\'évènement souhaité :</u>'.$datefin.'</p><br />
+
+               <br />
+               '.nl2br($message).'
+               <br />
+               <!-- mettez des addresse d\'image sur internet -->
+               
+            </div>
+         </body>
+      </html>
+      ';
+      /*on fait appel à la fonction mail de php*/
+      mail("contact@arb-performances.fr", "Message provenant d'ARB-Performances.fr", $message, $header);
+      $msg="Votre message a bien été envoyé !";
+      $name = $lastname = $email = $phone = $datedeb = $datefin = $message = "";
+   } else { 
+       // sinon on ecrit ce message si tous les champs ne sont pas complété
+      $msg="Tous les champs doivent être complétés !";
+   }
+
+}
+
+   /* Nous allons créer une fonction qui vérifie les inputs et les néttois pour éviter de se faire hacker */
+   function verificationInput($var)
+   {
+    //Supprime les espaces (ou d'autres caractères) en début et fin de chaîne
+    $var = trim($var);
+   
+    //Supprime les antislashs d'une chaîne
+   $var = stripslashes($var);
+    
+    //Convertit les caractères spéciaux en entités HTML
+    $var = htmlspecialchars($var);
+    return $var;
+   
+   }
+   
+   function verificationEmail($var)
+   {
+     return filter_var($var, FILTER_VALIDATE_EMAIL);
+   }
+?>
---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>ARB-Performances</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--ICONES-->
+
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+    <!--STYLES-->
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" media="screen and (min-width: 901px)" href="../CSS/StyleDesktop.css">
+    <link rel="stylesheet" media="screen and (max-width: 900px)" href="../CSS/StyleTablette.css" />
+    <link rel="stylesheet" media="screen and (max-width: 700px)" href="../CSS/StyleMobile.css">
+
+    <!--FONTS-->
+
+    <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet">
+
+</head>
+
+<!--HEADER
+========================================-->
+
+<body>
+    <section id="navbar">
+        <div class="header">
+            <div class="logo">
+                <a href="index.html"><img src="../ressources/images/logo_arb/logoarb.JPG"></a>
+            </div>
+            <div id="rectangle">
+                <nav role="navigation">
+                    <div id="menuToggle">
+                        <input type="checkbox" />
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <ul id="menu">
+                            <li><a href="Actualites.html">ACTUALITÉS</a></li>
+                            <li><a href="Partenaires.html">PARTENAIRES</a></li>
+                            <li><a href="Team.html">LE TEAM</a></li>
+                            <li><a href="Calendrier.html">CALENDRIER</a></li>
+                            <li><a href="Moto.html">MOTO</a></li>
+                            <li><a href="Galerie.html">GALERIE</a></li>
+                            <li><a href="Boutique.html">BOUTIQUE</a></li>
+                            <li><a href="Adhesions.html">ADHESIONS & DONS</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+        </div>
+        </div>
+    </section>
+
+    <!-- ACCUEIL
+========================================-->
+
+    <section id="accueil">
+
+        <h1 data-aos="fade-up" data-aos-duration="3000">Team engagé en championnat de France endurance</h1>
+
+        <section data-aos="fade-up" data-aos-duration="3000" class="course course1">
+            <img class="tracks" src="../ressources/images/Tracks/dijon_track.jpg" alt="CircuitDijon">
+            <div class="compte_a_rebours_participe">
+                <div class="compte_a_rebours"><noscript>Fin de l'évènement le 15 mai 2021.</noscript></div>
+                <a href="contact.php"><button class="btn">Je Participe</button></a>
+            </div>
+            </div>
+            <img class="logo_course" src="../ressources/images/Logo_orgnasime_course/ultimate_logo.JPG"
+                alt="ultimate cup">
+        </section>
+
+    </section>
+
+    <!-- ACTUALITES
+========================================-->
+
+    <h2 data-aos="fade-up" data-aos-duration="3000" style="text-decoration: underline;">Nos dernières actualités</h2>
+
+    <!--actualités 1-->
+
+    <section data-aos="fade-up" data-aos-duration="3000" class="actu actu1">
+        <h4>12eme position pour les 12h de Magny-Cours!</h4>
+        <p>Après plusieurs mois de préparation, l’équipe ARB-Performance ramène la 12° position lors des 12h de
+            Magny-Cours 2019.
+            Manche du Championnat d’Europe Open d’Endurance, ce rendez-vous aura été une belle page d’apprentissage pour
+            l’équipe qui participait pour la première fois à un si haut niveau de la compétition.</p>
+        <button class="btn">Lire la suite</button>
+    </section>
+
+    <!--actualités 2-->
+
+    <section data-aos="fade-up" data-aos-duration="3000" class="actu actu2">
+        <h4>TOP 10 Nogaro</h4>
+        <p>Premier Top 10 pour l’équipe ARB-Performances lors de la seconde manche du Championnat de France d’Endurance.
+            Sur le circuit de Nogaro, l’équipe connaîtra un weekend sans problème qui permettra d’accéder au Top 10 du
+            général mais aussi à la troisième place des équipages à la française. Good job les gars !</p>
+        <button class="btn">Lire la suite</button>
+    </section>
+
+    <!--button plus d'actualités-->
+
+    <section class="plus_actu">
+        <div class="trait_G"></div>
+        <button class="btn btn2">Plus d'actu</button>
+        <div class="trait_D"></div>
+    </section>
+
+    <!-- SOCIAL
+========================================-->
+
+    <section class="social">
+        <h3>RESTEZ INFORMÉS</h3>
+        <div class="logo_reseaux_sociaux">
+            <div class="facebook">
+                <a href="#"><img src="../ressources/icones/fb_logo.svg"></a>
+            </div>
+            <div class="separator"></div>
+            <div class="instagram">
+                <a href="#"><img src="../ressources/icones/insta_logo.svg" alt=""></a>
+            </div>
+        </div>
+        <div class="contact-form">
+            <label>Inscrivez-vous à la newsletter : </label>
+            <input type="email" id="email" placeholder="Votre adresse email">
+            <button class="btn">Envoyer</button>
+        </div>
+    </section>
+
+    <!-- SPONSORS
+========================================-->
+
+    <h4>Merci à nos pricipaux sponsors !</h4>
+
+    <section id="sponsors_logo">
+        <img src="../ressources/images/sponsors/PGC.jpg" alt="pgc">
+        <img src="../ressources/images/sponsors/CREALIS.jpg" alt="crealis">
+        <img src="../ressources/images/sponsors/FOST.jpg" alt="fost">
+        <img src="../ressources/images/sponsors/Bsearch.jpg" alt="bsearch">
+        <img src="../ressources/images/sponsors/Milton-Services.jpg" alt="miltonservices">
+        <img src="../ressources/images/sponsors/honda.png" alt="honda">
+        <img src="../ressources/images/sponsors/3AS-Racing.jpg" alt="3AS-Racing">
+    </section>
+
+    <!--FOOTER
+=======================================-->
+    <section id="footer">
+        <h5>N'hésitez pas à contacter l'équipe ARB-Performances
+            <br><a href="#">contact@arb-perfomances.fr</a>
+            <br>--</h5>
+        <h6>Dévoloppé par <a href="RomainMirial.fr">RomainMirial.fr</a> - 2021</h6>
+    </section>
+
+    <!--JAVASCRIPT-->
+
+    <script type="text/javascript" src="../Javascript/App.js"></script>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script>
+        AOS.init();
+    </script>
+
+</body>
+
+</html>
